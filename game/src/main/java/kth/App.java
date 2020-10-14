ab4357c17ec4a0c514d656f66428660a7e5c865b
Hazard <hazard9909@gmail.com>
2020-10-14 18:10:16 +0200

    Only redraw BoardView

--- game/src/main/java/kth/App.java
+++ game/src/main/java/kth/App.java
@@ -8,6 +8,7 @@ import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.stage.*;
+import kth.controllers.MenuController;
 import kth.models.BoardModel;
 import kth.views.BoardView;
 
@@ -15,33 +16,22 @@ import kth.views.BoardView;
  * JavaFX App
  */
 public class App extends Application {
+    public static final int TILE_SIZE = 100;
+    public static final int WIDTH = 8;
+    public static final int HEIGHT = 8;
     private static App app;
+    private BoardView boardview;
+    private Stage stage;
+    private VBox vbox;
+
     public static App get() {
         return app;
     }
     
-    private Stage stage;
-    public static final int TILE_SIZE = 100;
-    public static final int WIDTH = 8;
-    public static final int HEIGHT = 8;
-
     public void draw() {
-        stage.setTitle("Turkisk Dam");
-
-        MenuBar menuBar = new MenuBar();
-        {
-            Menu menu = new Menu("Game");
-            MenuItem restart = new MenuItem("Restart");
-            menu.getItems().add(restart);
-            menuBar.getMenus().add(menu);
-        }
-        
-        VBox box = new VBox(menuBar);
-        var boardview = new BoardView(BoardModel.get());
-        box.getChildren().add(boardview);
-        Scene scene = new Scene(box);
-        stage.setScene(scene);
-        
+        vbox.getChildren().remove(boardview);
+        boardview = new BoardView(BoardModel.get());
+        vbox.getChildren().add(boardview);
         stage.show();
     }
 
@@ -53,6 +43,24 @@ public class App extends Application {
     public void start(Stage stage) throws Exception {
         app = this;
         this.stage = stage;
+
+        stage.setTitle("Turkisk Dam");
+
+        MenuBar menuBar = new MenuBar();
+        {
+            Menu menu = new Menu("Game");
+            MenuItem restart = new MenuItem("Restart");
+            menu.getItems().add(restart);
+            menuBar.getMenus().add(menu);
+        }
+
+        vbox = new VBox(menuBar);
+        boardview = new BoardView(BoardModel.get());
+        vbox.getChildren().add(boardview);
+        
+        Scene scene = new Scene(vbox);
+        stage.setScene(scene);
+        
         draw();
     }
 }
